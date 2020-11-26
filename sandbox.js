66af7486600442e97e156250168b76bec7385121
git-shashwat <shashwat824@gmail.com>
2020-11-26 14:14:13 +0530

    setup & compilation

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var str = 'luigi';
+console.log(str);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const str = 'luigi';
+
+console.log(str); 
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input)
+})
