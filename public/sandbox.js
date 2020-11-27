237778982752dc217b545a25909e539e0fbe2459
git-shashwat <shashwat824@gmail.com>
2020-11-27 14:15:00 +0530

    type aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,16 +1,9 @@
 "use strict";
-var greet;
-// greet = 'hello';
-greet = function () {
-    console.log("hello", "again");
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
+var greet = function (user) {
+    console.log(user.name + " says hello");
 };
-add(5, 10, "25");
-var minus = function (a, b) {
-    return a - b;
-};
-var result = minus(10, 7);
+logDetails(123, "coke");
+greet({ name: "suresh", uid: 234 });
