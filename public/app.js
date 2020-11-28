3a7aed8a766c19b034b86a67c71223971958534b
git-shashwat <shashwat824@gmail.com>
2020-11-28 17:58:15 +0530

    rendering a HTML template

--- public/app.js
+++ public/app.js
@@ -1,38 +1,27 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice("mario", "work on the mario website", 420);
-// docTwo = new Payment("luigi", "work on the luigi website", 69);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice("mario", "work on the mario website", 420);
-// const invTwo = new Invoice("luigi", "work on the luigi website", 69);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
 const form = document.querySelector(".new-item-form");
 //inputs
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
     const t = tofrom.value;
     const d = details.value;
     const a = amount.valueAsNumber;
-    if (type.value === "invoice") {
+    const h = type.value;
+    if (h === "invoice") {
         doc = new Invoice(t, d, a);
     }
     else {
         doc = new Payment(t, d, a);
     }
-    console.log(doc);
+    list.render(doc, h, "start");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,7 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("mario", "work on the mario website", 420);
-// docTwo = new Payment("luigi", "work on the luigi website", 69);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const invOne = new Invoice("mario", "work on the mario website", 420);
-// const invTwo = new Invoice("luigi", "work on the luigi website", 69);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach((inv) => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -33,6 +11,10 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
@@ -40,11 +22,12 @@ form.addEventListener("submit", (e: Event) => {
   const t: string = tofrom.value;
   const d: string = details.value;
   const a: number = amount.valueAsNumber;
-  if (type.value === "invoice") {
+  const h: string = type.value;
+  if (h === "invoice") {
     doc = new Invoice(t, d, a);
   } else {
     doc = new Payment(t, d, a);
   }
 
-  console.log(doc);
+  list.render(doc, h, "start");
 });
